d217cd8b56d7b83dd81234cfe8b844b39451ff13
Mohamed Massoud <104199376+MohamedMassoud10@users.noreply.github.com>
2022-08-24 08:10:36 -0700

    fix some problems

--- src/component/Start.js
+++ src/component/Start.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Data from "./Data";
 import { Link } from "react-router-dom";
-export default function Start({interval }) {
+export default function Start({interval ,setIntervall }) {
     function handelClick() {
         interval();
+        setIntervall(15)
     }
     return (
         <div className="start">
